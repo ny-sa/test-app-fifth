95b8b970daa142277f74adf2b61c931f4ff58723
nysa <kusogedump@gmail.com>
2020-02-12 18:33:48 +0100

    database upgrade

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://nysa:12345@cluster0-cz3ei.mongodb.net/blogDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  post: String
+});
+
+const Post = mongoose.model('Post', postSchema);
 
 const homeStartingContent = 'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
 const aboutContent = 'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.';
@@ -11,7 +24,7 @@ const contactContent = 'Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-const posts = [];
+//const posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -19,7 +32,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('home', {eHomeContent: homeStartingContent, ePosts: posts});
+  Post.find({}, (err, allPosts) => {
+    if (!err) 
+      res.render('home', {
+        eHomeContent: homeStartingContent, 
+        ePosts: allPosts
+      });
+  })
 });
 
 app.get('/about', (rew, res) => {
@@ -34,20 +53,26 @@ app.get('/compose', (req, res) => {
   res.render('compose');
 })
 
-app.get('/posts/:title', (req, res) => {
-  posts.forEach(e => {
-    if(_.kebabCase(e.title) === _.kebabCase(req.params.title))
+app.get('/posts/:id', (req, res) => {
+  Post.findOne({_id: req.params.id}, (err, onePost) => {
+    if (!err)
+      res.render('post', {ePostTitle: onePost.title, ePostPost: onePost.post})
+  });
+  /*posts.forEach(e => {
+    if(_.lowerCase(e.title) === _.lowerCase(req.params.title))
       res.render('post', {ePostTitle: e.title, ePostPost: e.post})
     else console.log('Match not found!')
-  })
+  })*/
 })
 
 app.post('/compose', (req, res) => {
-  posts.push({
+  const newPost = new Post({
     title: req.body.postTitle,
     post: req.body.postPost
+  })
+  newPost.save((err) => {
+    if (!err) res.redirect('/');
   });
-  res.redirect('/');
 })
 
 app.listen(process.env.PORT || 2020, function() {
